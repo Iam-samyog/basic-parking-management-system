13c8f3f09f1f897580b75985905ea5fc38409239
Iam-samyog <msamyog37@gmail.com>
2024-12-10 22:54:21 +0545

    Maked some changes on the delete tab with integer

--- deletedb.py
+++ deletedb.py
@@ -4,7 +4,7 @@ connection=sqlite3.connect('parking.db')
 
 cursor=connection.cursor()
 
-delete_id=input("Enter the id of the vehcile you want to delete:")
+delete_id=int(input("Enter the id of the vehcile you want to delete:"))
 
 cursor.execute(
     "DELETE FROM parking WHERE id=?",(delete_id,)
